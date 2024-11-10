ca02cd71574128875138b4bbce30fc924d5a4e63
Jihyuk Lee <lead@sver.dev>
2024-11-10 22:07:11 +0900

    feat :: 스케줄러를 통해 예약된 전체 세션에 메시지 전송

--- core/core-api/src/main/kotlin/io/dodn/springboot/CoreApiApplication.kt
+++ core/core-api/src/main/kotlin/io/dodn/springboot/CoreApiApplication.kt
@@ -3,9 +3,11 @@ package io.dodn.springboot
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableScheduling
 class CoreApiApplication
 
 fun main(args: Array<String>) {
