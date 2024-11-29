3413a06e82f063e4f24efa3795b8682bf806bed0
Jihyuk Lee <lead@sver.dev>
2024-11-29 10:48:07 +0900

    chore :: 주석 제거

--- storage/db-core/src/main/kotlin/io/dodn/springboot/storage/db/core/entity/UserEntity.kt
+++ storage/db-core/src/main/kotlin/io/dodn/springboot/storage/db/core/entity/UserEntity.kt
@@ -1,10 +1,10 @@
-// package io.dodn.springboot.storage.db.core.entity
-//
-// import jakarta.persistence.Column
-// import jakarta.persistence.Entity
-//
-// @Entity
-// class UserEntity(
-//    @Column
-//    val exampleColumn: String,
-// ) : BaseEntity()
+package io.dodn.springboot.storage.db.core.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+
+@Entity
+class UserEntity(
+    @Column
+    val exampleColumn: String,
+) : BaseEntity()
--- storage/db-core/src/main/kotlin/io/dodn/springboot/storage/db/core/repository/ExampleRepository.kt
+++
@@ -1,5 +0,0 @@
-@file:Suppress("ktlint:standard:no-empty-file")
-
-package io.dodn.springboot.storage.db.core.repository
-
-// interface ExampleRepository : JpaRepository<ExampleEntity, Long>
