5e3685f5ffddd1903575eff810361e02b8281a61
Jihyuk Lee <lead@sver.dev>
2024-12-30 12:15:16 +0900

    fix :: 스케줄러 비활성화

--- core/core-api/src/main/kotlin/io/dodn/springboot/core/api/scheduler/SendScheduledMessage.kt
+++ core/core-api/src/main/kotlin/io/dodn/springboot/core/api/scheduler/SendScheduledMessage.kt
@@ -1,30 +1,11 @@
 package io.dodn.springboot.core.api.scheduler
 
-import io.dodn.springboot.core.api.config.WebSocketHandler.Companion.sessionList
-import io.dodn.springboot.core.api.dto.MessageDto
-import io.dodn.springboot.core.api.dto.MessageType
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import org.springframework.web.socket.TextMessage
 
 @Component
 class SendScheduledMessage {
     @Scheduled(fixedDelay = 5000)
     fun sendMessageByScheduled() {
-        sessionList.forEach { i ->
-            i.sendMessage(
-                TextMessage(
-                    MessageDto(
-                        content = "10초마다 핑",
-                        type = MessageType.PING,
-                        userId = 0,
-                        channel = 0,
-                        messageId = 0,
-                        parentMessageId = 0,
-                        isBlind = false,
-                    ).toString(),
-                ),
-            )
-        }
     }
 }
