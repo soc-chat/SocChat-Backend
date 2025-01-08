eb369e3e7e9f278860a3a02130962260ca6ea553
Jihyuk Lee <lead@sver.dev>
2025-01-08 20:10:05 +0900

    fix :: 웹소켓 활성화

--- core/core-api/src/main/kotlin/io/dodn/springboot/core/api/config/StompHandler.kt
+++ core/core-api/src/main/kotlin/io/dodn/springboot/core/api/config/StompHandler.kt
@@ -2,9 +2,11 @@ package io.dodn.springboot.core.api.config
 
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.stereotype.Component
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
+@EnableWebSocketMessageBroker
 @Component
 class StompHandler : WebSocketMessageBrokerConfigurer {
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
