6c01b5e46b08a80da64d0671c31f9b988ac72e18
Jihyuk Lee <lead@sver.dev>
2025-01-08 16:01:11 +0900

    chore :: 기타 수정

--- core/core-api/src/main/kotlin/io/dodn/springboot/core/api/config/StompHandler.kt
+++ core/core-api/src/main/kotlin/io/dodn/springboot/core/api/config/StompHandler.kt
@@ -6,7 +6,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Component
-class WebSocketHandler : WebSocketMessageBrokerConfigurer {
+class StompHandler : WebSocketMessageBrokerConfigurer {
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry.enableSimpleBroker("/sub")
         registry.setApplicationDestinationPrefixes("/pub")
