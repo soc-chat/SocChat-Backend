78a6c2ae234f618a573fbbbe3b02a77db6f119d5
Jihyuk Lee <lead@sver.dev>
2025-01-15 01:04:07 +0900

    chore :: 중복 메시징 제거

--- core/core-api/src/main/kotlin/io/dodn/springboot/core/api/controller/StompController.kt
+++ core/core-api/src/main/kotlin/io/dodn/springboot/core/api/controller/StompController.kt
@@ -3,17 +3,14 @@ package io.dodn.springboot.core.api.controller
 import io.dodn.springboot.core.api.dto.MessageDto
 import io.dodn.springboot.core.api.service.ChatService
 import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.simp.SimpMessageSendingOperations
 import org.springframework.stereotype.Controller
 
 @Controller
 class StompController(
     private val chatService: ChatService,
-    private val simpMessageSendingOperations: SimpMessageSendingOperations,
 ) {
     @MessageMapping("/send")
     private fun sendMessage(message: MessageDto) {
         chatService.publishMessage(message)
-        simpMessageSendingOperations.convertAndSend("/sub/chat/room/${message.channel}", message)
     }
 }
