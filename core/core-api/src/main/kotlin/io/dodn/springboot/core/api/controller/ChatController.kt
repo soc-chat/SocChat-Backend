5e28840d2c30894bd668675239bbae17fa4e9538
Jihyuk Lee <lead@sver.dev>
2025-01-15 12:16:14 +0900

    feat :: 채팅방 정보 API 추가

--- core/core-api/src/main/kotlin/io/dodn/springboot/core/api/controller/ChatController.kt
+++ core/core-api/src/main/kotlin/io/dodn/springboot/core/api/controller/ChatController.kt
@@ -1,20 +1,19 @@
 package io.dodn.springboot.core.api.controller
 
+import io.dodn.springboot.core.api.dto.ChatRoomDto
+import io.dodn.springboot.core.api.service.ChatService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ChatController {
-//    @PostMapping("/rooms")
-//    fun createRoom(@RequestBody body: CreateRoomDto) {
-//    }
+class ChatController(private val chatService: ChatService) {
+    @GetMapping("/room")
+    fun findAllRooms(): ResponseEntity<List<ChatRoomDto>> =
+        ResponseEntity.ok(chatService.availableChatRoomList().map { ChatRoomDto.from(it) })
 
-    @GetMapping("/rooms")
-    fun findAllRooms() {
-    }
-
-    @GetMapping("/chat")
-    fun getLastChatting(@RequestParam roomId: Long) {
-    }
+    @GetMapping("/room/{roomId}")
+    fun getLastChatting(@PathVariable roomId: Long): ResponseEntity<ChatRoomDto> =
+        ResponseEntity.ok(ChatRoomDto.from(chatService.findChatRoom(roomId)))
 }
