3e9be959485241a5356fcc98f21eb93bb1f70705
Jihyuk Lee <lead@sver.dev>
2025-01-25 03:13:02 +0900

    chore :: 코드 스타일 통합

--- storage/db-core/src/main/kotlin/io/dodn/springboot/storage/db/core/repository/ChatRoomRepository.kt
+++ storage/db-core/src/main/kotlin/io/dodn/springboot/storage/db/core/repository/ChatRoomRepository.kt
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDateTime
 
 interface ChatRoomRepository : JpaRepository<ChatRoomEntity, Long> {
-    fun findByExpireTimeBefore(expire: LocalDateTime): List<ChatRoomEntity>
+    fun findByStartTimeAfterAndExpireTimeBefore(
+        startTime: LocalDateTime,
+        expireTime: LocalDateTime,
+    ): List<ChatRoomEntity>
 }
