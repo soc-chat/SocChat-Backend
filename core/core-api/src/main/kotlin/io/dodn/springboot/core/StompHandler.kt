71c3b425a97aa7f50f6d256353e14e302113ec9a
Jihyuk Lee <lead@sver.dev>
2025-01-25 03:12:48 +0900

    feat :: 초당 채팅 수 제한 추가

---
+++ core/core-api/src/main/kotlin/io/dodn/springboot/core/StompHandler.kt
@@ -0,0 +1,32 @@
+package io.dodn.springboot.core
+
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.http.server.ServletServerHttpRequest
+import org.springframework.web.socket.WebSocketHandler
+import org.springframework.web.socket.server.HandshakeInterceptor
+import java.lang.Exception
+
+class StompHandler : HandshakeInterceptor {
+
+    override fun beforeHandshake(
+        request: ServerHttpRequest,
+        response: ServerHttpResponse,
+        wsHandler: WebSocketHandler,
+        attributes: MutableMap<String, Any>,
+    ): Boolean {
+        if (request is ServletServerHttpRequest) {
+            val session = request.servletRequest.session
+            attributes["sessionId"] = session.id
+        }
+        return true
+    }
+
+    override fun afterHandshake(
+        request: ServerHttpRequest,
+        response: ServerHttpResponse,
+        wsHandler: WebSocketHandler,
+        exception: Exception?,
+    ) {
+    }
+}
---
+++ core/core-api/src/main/kotlin/io/dodn/springboot/core/service/BucketService.kt
@@ -0,0 +1,22 @@
+package io.dodn.springboot.core.service
+
+import io.github.bucket4j.Bandwidth
+import io.github.bucket4j.Bucket
+import io.github.bucket4j.Refill
+import org.springframework.stereotype.Service
+import java.time.Duration
+import java.util.concurrent.ConcurrentHashMap
+
+@Service
+class BucketService {
+    private val cache: MutableMap<String, Bucket> = ConcurrentHashMap<String, Bucket>()
+
+    fun resolveBucket(sessionId: String): Bucket =
+        cache.computeIfAbsent(sessionId) { this.newBucket(sessionId) }
+
+    private fun newBucket(sessionId: String): Bucket =
+        Bucket
+            .builder()
+            .addLimit(Bandwidth.classic(5, Refill.intervally(1, Duration.ofSeconds(1))))
+            .build()
+}
