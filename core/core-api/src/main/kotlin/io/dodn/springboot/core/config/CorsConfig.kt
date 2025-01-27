3314171d0bd8c9ad25294b7a369f64a364921a08
Jihyuk Lee <lead@sver.dev>
2025-01-28 00:51:32 +0900

    hotfix :: 상속 변경

--- core/core-api/src/main/kotlin/io/dodn/springboot/core/config/CorsConfig.kt
+++ core/core-api/src/main/kotlin/io/dodn/springboot/core/config/CorsConfig.kt
@@ -1,26 +1,16 @@
 package io.dodn.springboot.core.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class CorsConfig {
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val config = CorsConfiguration()
-
-        config.allowCredentials = false
-        config.allowedOrigins = mutableListOf("*")
-        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-        config.exposedHeaders = listOf("Authorization", "Content-Type", "Accept")
-        config.allowedHeaders = listOf("Authorization", "Content-Type", "Accept")
-
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", config)
-
-        return source
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET")
+            .maxAge(3600)
     }
 }
